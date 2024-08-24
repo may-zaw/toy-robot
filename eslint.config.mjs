e3fcfa48322cd95f8b9b5dc55e731ae75641de56
May Zaw <mzaw@sesimi.com>
2024-08-24 13:43:45 +1000

    update eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,23 @@
-import globals from 'globals'
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import globals from "globals"
+import tseslint from "typescript-eslint"
 import stylisticTs from '@stylistic/eslint-plugin-ts'
 
 export default [
-  {files: [`**/*.{js,mjs,cjs,ts}`]},
-  {languageOptions: { globals: globals.browser }},
   {
+    files: [`**/*.{js,mjs,cjs,ts}`],
+    languageOptions: { globals: globals.browser },
+    ignores: [`**/dist/**/*.js`],
     plugins: {
-      '@stylistic/ts': stylisticTs
+      "@stylistic/ts": stylisticTs
     },
     rules: {
-      'semi': [`error`, `never`],
-      'indent': [`error`, 2],
-      '@stylistic/ts/indent': [`error`, 2],
-      "no-unused-vars": "warn",
-      quotes: [`error`, `backtick`],
+      "semi": [`error`, `never`],
+      "indent": `off`,
+      "@stylistic/ts/indent": [`error`, 2],
+      "no-unused-vars": `warn`,
+      "@typescript-eslint/no-require-imports": `off`, // Disable this rule
+      "quotes": [`error`, `backtick`],
     }
   },
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ]
